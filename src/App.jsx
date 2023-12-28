b2d66ff1488425b60fec683a5a0d20d77525cdeb
Mnnan Mazhar <128976503+mnnandev@users.noreply.github.com>
2023-12-28 15:44:01 +0500

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { Box } from "@mui/material";
 import React from "react";
 import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Feed, SearchFeed, VideoDetail, Channel, Navbar } from "./Components";
-
+// import { Feed, SearchFeed, VideoDetail, Channel, Navbar } from "./Components";
+import Feed from './Components/Feed'
+import VideoDetail from "./Components/VideoDetail"
+import Channel from "./Components/Channel"
+import SearchFeed from "./Components/SearchFeed"
+import Navbar from "./Components/Navbar";
 const App = () => (
   <BrowserRouter>
     <Box sx ={{backgroundColor:"#000"}}>
